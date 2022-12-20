91c7008b149279d0123004c7ce877311572baf45
Artem LAPOV <artemlapov@me.com>
2022-12-20 18:56:30 +0100

    Initial Commit

---
+++ SliderApp(SwiftUI and UIkit)/SliderApp_SwiftUI_and_UIkit_App.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp_SwiftUI_and_UIkit_App.swift
+//  SliderApp(SwiftUI and UIkit)
+//
+//  Created by Artem Lapov on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp_SwiftUI_and_UIkit_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
